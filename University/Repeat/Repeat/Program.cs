043d9f3a614779140cfb0e9db1aaca575342bd4a
levoshka24 <114833553+levoshka24@users.noreply.github.com>
2023-02-09 22:48:52 +0300

    init

---
+++ University/Repeat/Repeat/Program.cs
@@ -0,0 +1,20 @@
+﻿int n, k;
+Console.WriteLine("Введите сумму");
+n = int.Parse(Console.ReadLine());
+k = 64;
+Console.WriteLine("Найкратчайшее количство купюр которое вам надо будет отдать");
+while (n > 0)
+{
+    if (n >= k)
+    {
+        int z = n / k;
+        Console.WriteLine(z + "*" + k);
+        n = n % k;
+        k = k / 2;
+
+    }
+    
+    
+        
+    
+}
