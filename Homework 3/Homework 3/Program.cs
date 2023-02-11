20b8a6a4b7ca400b128175897131fee849ca24c0
levoshka24 <114833553+levoshka24@users.noreply.github.com>
2023-02-11 10:42:43 +0300

    init

--- Homework 3/Homework 3/Program.cs
+++ Homework 3/Homework 3/Program.cs
@@ -1,23 +1,31 @@
 ﻿// TASK 2
-string line = "виктор мыл пилораму";
-string[] subs = line.Split(' ');
-var sorted = subs.OrderBy(n => n.Length);
-var shortest = sorted.FirstOrDefault();
-var longest = sorted.LastOrDefault();
-Console.WriteLine($"The shortest-{shortest}");
-Console.WriteLine($"The longest-{longest}");
+//string line = "виктор мыл пилораму";
+//string[] subs = line.Split(' ');
+//var sorted = subs.OrderBy(n => n.Length);
+//var shortest = sorted.FirstOrDefault();
+//var longest = sorted.LastOrDefault();
+//Console.WriteLine($"The shortest-{shortest}");
+//Console.WriteLine($"The longest-{longest}");
 // TASK 3
-//string ln= "ffffff ghtg flgkdem";
-//string[] mass = line.Split(' ');
-//char l = Convert.ToChar(Console.ReadLine());
-//for(int i = 0; i < mass.Length; i++)
+//string str = "Techie Delight";
+//string[] mass = str.Split(' ');
+//char ch = Convert.ToChar(Console.ReadLine());
+//int freq = str.Count(f => (f == ch));
+// TASK 4
+//string original = Console.ReadLine();
+//var reversed = new string(original.Reverse().ToArray());
+//Console.WriteLine(reversed);
+//var result = original.Equals(reversed);
+//if (result)
 //{
-//    int freq = mass.Count(f => (f == l));
-//    Console.WriteLine(freq);
+//    Console.WriteLine("палиндром");
 //}
-
-string str = "Techie Delight";
-string[] mass = str.Split(' ');
-char ch = Convert.ToChar(Console.ReadLine());
-int freq = str.Count(f => (f == ch));
-Console.WriteLine(freq);
+//else
+//{
+//    Console.WriteLine("не палиндром");
+//}
+//Console.WriteLine(freq);
+// TASK 5
+//string name = Console.ReadLine();
+//string result = string.Join("", name.Select(x => new string(x, 2)));
+//Console.WriteLine(result);
