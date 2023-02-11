935d2e3936fbeca4e3acf543f89af9306ae5753c
levoshka24 <114833553+levoshka24@users.noreply.github.com>
2023-02-11 10:26:11 +0300

    init

--- Homework 3/Homework 3/Program.cs
+++ Homework 3/Homework 3/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// TASK 2
+string line = "виктор мыл пилораму";
+string[] subs = line.Split(' ');
+var sorted = subs.OrderBy(n => n.Length);
+var shortest = sorted.FirstOrDefault();
+var longest = sorted.LastOrDefault();
+Console.WriteLine($"The shortest-{shortest}");
+Console.WriteLine($"The longest-{longest}");
+// TASK 3
+//string ln= "ffffff ghtg flgkdem";
+//string[] mass = line.Split(' ');
+//char l = Convert.ToChar(Console.ReadLine());
+//for(int i = 0; i < mass.Length; i++)
+//{
+//    int freq = mass.Count(f => (f == l));
+//    Console.WriteLine(freq);
+//}
+
+string str = "Techie Delight";
+string[] mass = str.Split(' ');
+char ch = Convert.ToChar(Console.ReadLine());
+int freq = str.Count(f => (f == ch));
+Console.WriteLine(freq);
