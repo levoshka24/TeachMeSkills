9933f221681936ac102e8bf06db49e8331493e00
levoshka24 <114833553+levoshka24@users.noreply.github.com>
2023-02-15 20:05:17 +0300

    init

---
+++ OOP-HW/OOP-HW/PhoneClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace OOP_HW
+{
+    public class PhoneClass
+    {
+        public string number;
+        public double weight;
+        public string model;
+        
+
+        public PhoneClass(string numberInit, double weightInit, string modelInit)
+        {
+            number = numberInit;
+            weight = weightInit;
+            model = modelInit;
+        }
+
+        
+    }
+}
---
+++ OOP-HW/OOP-HW/Program.cs
@@ -0,0 +1,5 @@
+﻿using OOP_HW;
+
+PhoneClass phone1 = new PhoneClass("+375441295746",6.5,"iphone");
+PhoneClass phone2 = new PhoneClass("+375295968576",7.4,"samsung");
+PhoneClass phone3 = new PhoneClass("+375336859634",6.5,"huawei");
