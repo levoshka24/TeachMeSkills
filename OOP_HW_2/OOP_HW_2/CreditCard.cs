c9028c071d45c50efb6c584574e4a6b8c13a7bc2
levoshka24 <114833553+levoshka24@users.noreply.github.com>
2023-02-17 19:03:02 +0300

    init

---
+++ OOP_HW_2/OOP_HW_2/CreditCard.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_HW_2
+{
+    public class CreditCard
+    {
+        public long number = 52208130004775387;
+        public long cash = 5000;
+        public void Add_Money(int add_money)
+        {
+            
+            
+            long d = cash + add_money;
+            Console.WriteLine($"Теперь на счету - {d}");
+        }
+        public void Take_Cash(int take_money)
+        {
+            
+            long d = cash - take_money;
+            Console.WriteLine($"Теперь на счету - {d}");
+        }
+    }
+    
+}
---
+++ OOP_HW_2/OOP_HW_2/Program.cs
@@ -0,0 +1,11 @@
+﻿using OOP_HW_2;
+
+CreditCard card_1 = new CreditCard();
+Console.WriteLine($"У вас на счету-{card_1.cash}");
+card_1.Add_Money(300);
+CreditCard card_2 = new CreditCard();
+Console.WriteLine($"У вас на счету-{card_2.cash}");
+card_2.Add_Money(500);
+CreditCard card_3 = new CreditCard();
+Console.WriteLine($"У вас на счету-{card_3.cash}");
+card_3.Take_Cash(500);
