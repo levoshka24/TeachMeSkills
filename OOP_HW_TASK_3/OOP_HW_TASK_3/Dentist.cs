1624028bcac7c568d8e98e60cd4344fa437c9939
levoshka24 <114833553+levoshka24@users.noreply.github.com>
2023-02-19 22:43:49 +0300

    init

---
+++ OOP_HW_TASK_3/OOP_HW_TASK_3/Dentist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_HW_TASK_3
+{
+    public class Dentist:Doctor
+    {
+        public override void Treat()
+        {
+            Console.WriteLine("вам нужно менять зубы");
+        }
+    }
+}
---
+++ OOP_HW_TASK_3/OOP_HW_TASK_3/Xirurg.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_HW_TASK_3
+{
+    internal class Xirurg
+    {
+    }
+}
