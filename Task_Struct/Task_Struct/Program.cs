3811b9dcb04befdaa589b3fbbfe6e2a0a9fb3903
levoshka24 <114833553+levoshka24@users.noreply.github.com>
2023-02-22 20:06:57 +0300

    init

---
+++ Task_Struct/Task_Struct/Program.cs
@@ -0,0 +1,5 @@
+﻿//Задача.Создать структуру UserInfo, Которая включает в себя Name Age Address Surname Login Password. Вывести поля структуры в консоль.
+using Task_Struct;
+
+UserInfo user_1 = new("Leon",63,"lupina street","khalmou","loloshkaxD",0493994);
+user_1.Description();
---
+++ Task_Struct/Task_Struct/Struct_userinfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Struct
+{
+    public struct UserInfo
+    {
+        public string Name;
+        public int Age;
+        public string Adress;
+        public string Surname;
+        public string Login;
+        public int Password;
+        public UserInfo(string Name,int Age,string Adress,string Surname,string Login,int Password)
+        {
+            this.Name = Name;
+            this.Age = Age;
+            this.Adress = Adress;
+            this.Surname = Surname;
+            this.Login = Login;
+            this.Password = Password;
+
+        }
+        public void Description()
+        {
+            Console.WriteLine($"Имя - {Name} Ворзраст - {Age} Адресс - {Adress} Фамилия - {Surname} Логин - {Login} Пароль - {Password}");
+        }
+    }
+}
