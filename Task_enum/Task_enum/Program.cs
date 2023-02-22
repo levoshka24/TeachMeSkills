4f426e53854cb397fbd61a99f44c15db685955a7
levoshka24 <114833553+levoshka24@users.noreply.github.com>
2023-02-22 19:54:12 +0300

    init

---
+++ Task_enum/Task_enum/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("n=");
+var n = int.Parse(Console.ReadLine());
+Console.WriteLine((DayOfWeek)((n - 1) % Enum.GetValues(typeof(DayOfWeek)).Length));
+
+public enum DayOfWeek
+{
+    Понедельник = 0,
+    Вторник = 1,
+    Среда = 2,
+    Четверг = 3,
+    Пятниццо = 4,
+    Суббота = 5,
+    Воскресенье = 6
+}
+
+
