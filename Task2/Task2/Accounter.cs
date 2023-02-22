3d5b297d02bc1883cd4a34be7135898fa9810a18
levoshka24 <114833553+levoshka24@users.noreply.github.com>
2023-02-22 19:50:14 +0300

    init

---
+++ Task2/Task2/Accounter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class Accounter:INameOfCharge
+    {
+        public void NameOfCharge()
+        {
+            Console.WriteLine("моя должность бугалтер");
+        }
+    }
+}
---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,7 @@
+﻿using Task2;
+Director director1 = new Director();
+director1.NameOfCharge();
+Worker worker1 = new Worker();
+worker1.NameOfCharge();
+Accounter acounter1 = new Accounter();
+acounter1.NameOfCharge();
