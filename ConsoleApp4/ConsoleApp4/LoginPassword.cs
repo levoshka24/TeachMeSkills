a93c2cfa2d2027e6dd6ece50aa8d464ec85dcb1e
levoshka24 <114833553+levoshka24@users.noreply.github.com>
2023-03-01 00:35:29 +0300

    init

---
+++ ConsoleApp4/ConsoleApp4/LoginPassword.cs
@@ -0,0 +1,64 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp4
+{
+    static public class LoginPassword
+    {
+
+        static public void InputLogin(string n)
+        {
+            if (n.Length < 20)
+            {
+                throw new LoginException("неверный формат логина");
+            }
+            for(int i = 0; i < n.Length; i++)
+            {
+                if (n[i] == ' ')
+                {
+                    throw new LoginException("неверный формат логина");
+                }
+            }
+            
+        }
+        //private string confirmPassword;
+        //public string Value { get; set; } = "";
+        //public string ConfirmPassword
+        //{
+        //    get { return confirmPassword; }
+        //    set
+        //    {
+        //        if (!value.Equals(Value))
+        //        {
+        //            throw new PaswordException("пароли не совпадают");
+        //        }
+        //        else
+        //        {
+        //            confirmPassword = value;
+        //        }
+        //    }
+        static public void PasswordInput(string n)
+        {
+            if (n.Length > 20)
+            {
+                throw new PaswordException("неверный формат пароля кол-во символом больше 20");
+            }
+            for (int i = 0; i < n.Length; i++)
+            {
+                if (n[i] == ' ')
+                {
+                    throw new PaswordException("неверный формат пароля т.к есть пробелы");
+                }
+            }
+            bool isNumeric = Regex.IsMatch(n, "[0-9]");
+            if (!isNumeric)
+            {
+
+                throw new PaswordException("неверный формат пароля т.к нет цифр");
+            }
+            
+
+        }
+       
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/PaswordException.cs
@@ -0,0 +1,8 @@
+﻿
+namespace ConsoleApp4
+{
+    class PaswordException:Exception 
+    {
+        public PaswordException(string message) : base(message) { }
+    }
+}
