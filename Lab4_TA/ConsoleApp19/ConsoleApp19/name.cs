ab30cdf2008b007dd400bb35f564bd66973a1041
levoshka24 <114833553+levoshka24@users.noreply.github.com>
2023-05-29 22:06:23 +0300

    init

---
+++ Lab4_TA/ConsoleApp19/ConsoleApp19/name.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    delegate void Del();
+    public abstract class Car
+    {
+        public abstract string Name { get; set; }
+        public abstract string Weight { get; set; 
+    }
+        public class Audi : Car
+        {
+            public override string Name { get; set; }
+            public override string Weight { get; set; }
+        }
+}
